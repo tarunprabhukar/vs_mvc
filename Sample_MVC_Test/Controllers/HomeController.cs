e4b981280d9a60e00100dff1a43f06960c8ccfb6
TKasala <TKasala@CTRTKASALA-L>
2016-10-17 11:47:20 -0400

    Initial Commit

---
+++ Sample_MVC_Test/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample_MVC_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Hello World, This is my first MVC App";
+        }
+    }
+}
